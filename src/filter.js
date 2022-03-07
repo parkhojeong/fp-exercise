20b1247c883aaabc7044c1e10452042214ab5da9
parkhojeong <parkhj062@gmail.com>
2022-03-07 23:10:51 +0900

    fix > filter

--- src/filter.js
+++ src/filter.js
@@ -1,7 +1,7 @@
 const filter = (list, predicate) => {
   let new_list = [];
   for(let i = 0, len = list.length; i < len; i++){
-      if(predicate(list[i])) new_list.push(list[i], i);
+      if(predicate(list[i],i)) new_list.push(list[i]);
   }
   return new_list;
 }
