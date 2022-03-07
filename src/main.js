e09c31a0652f6d666fdeff749c779db87f377071
parkhojeong <parkhj062@gmail.com>
2022-03-07 22:30:08 +0900

    bvalue

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 const filter = require('./filter')
 const map = require('./map')
+const bvalue = require('./bvalue')
 
 console.log(filter([1,2,3], (val)=> val>2))
 
 console.log(map([1,2,3], (val)=> val**2))
+
+console.log(bvalue('a')({a:'hi', b:'hello'}))
